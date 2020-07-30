a869e4201aad4e83e2857f8ef7e4cc2b64b5c8bd
Hardy Lutula Dylut <hardylutula@gmail.com>
2020-07-30 09:38:17 +0200

    basic server added

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", function (req, res) {
+  res.send("Hello #dylut2000!");
+});
+
+const server = app.listen(port, () => console.log(`http://localhost:${port}`));
